a639ed90d21a6471b5c8627d5c98fb9ab5898472
Renato Groffe <renato.groffe@yahoo.com.br>
2019-12-07 14:11:10 -0300

    Teste final

--- APIContagem/Controllers/ContadorController.cs
+++ APIContagem/Controllers/ContadorController.cs
@@ -23,7 +23,7 @@ namespace APIContagem.Controllers
                 {
                     _CONTADOR.ValorAtual,
                     Environment.MachineName,
-                    Local = "Azure na Prática",
+                    Local = "Azure na Prática - São Paulo-SP",
                     Sistema = Environment.OSVersion.VersionString,
                     Variavel = configuration["TesteAmbiente"],
                     TargetFramework = Assembly
