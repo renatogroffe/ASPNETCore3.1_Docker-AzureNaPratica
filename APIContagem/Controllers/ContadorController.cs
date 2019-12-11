9405d89754bb69ca9e73dcc6104f6c48af127666
Renato Groffe <renato.groffe@yahoo.com.br>
2019-12-10 21:37:14 -0300

    Alteração no Controller

--- APIContagem/Controllers/ContadorController.cs
+++ APIContagem/Controllers/ContadorController.cs
@@ -23,7 +23,7 @@ namespace APIContagem.Controllers
                 {
                     _CONTADOR.ValorAtual,
                     Environment.MachineName,
-                    Local = "Vila Olímpia",
+                    Local = "Prédio da XP - Vila Olímpia",
                     Sistema = Environment.OSVersion.VersionString,
                     Variavel = configuration["TesteAmbiente"],
                     TargetFramework = Assembly
