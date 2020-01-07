1512fe86f1569f5e553d9593b10243e1ef8cccc6
Renato Groffe <renato.groffe@yahoo.com.br>
2020-01-06 22:33:48 -0300

    ASP.NET Core 3.1 + API REST + Docker + Alpine Linux + Docker Compose

--- APIContagem/Controllers/ContadorController.cs
+++ APIContagem/Controllers/ContadorController.cs
@@ -23,7 +23,7 @@ namespace APIContagem.Controllers
                 {
                     _CONTADOR.ValorAtual,
                     Environment.MachineName,
-                    Local = "Prédio da XP - Vila Olímpia",
+                    Local = "GitHub",
                     Sistema = Environment.OSVersion.VersionString,
                     Variavel = configuration["TesteAmbiente"],
                     TargetFramework = Assembly
