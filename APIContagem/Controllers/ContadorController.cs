4681a97e445b00678aaaad611ed87112be102ce3
Renato Groffe <renato.groffe@yahoo.com.br>
2020-01-07 03:08:36 -0300

    Build e Deployment Automatizado

--- APIContagem/Controllers/ContadorController.cs
+++ APIContagem/Controllers/ContadorController.cs
@@ -23,7 +23,7 @@ namespace APIContagem.Controllers
                 {
                     _CONTADOR.ValorAtual,
                     Environment.MachineName,
-                    Local = "GitHub - Teste Build Automatizado",
+                    Local = "GitHub - Teste Build e Deployment Automatizado",
                     Sistema = Environment.OSVersion.VersionString,
                     Variavel = configuration["TesteAmbiente"],
                     TargetFramework = Assembly
