61d8ad235457956c691f89387f054ed6f3879516
Renato Groffe <renato.groffe@yahoo.com.br>
2020-01-21 18:00:19 -0300

    Update ContadorController.cs

--- APIContagem/Controllers/ContadorController.cs
+++ APIContagem/Controllers/ContadorController.cs
@@ -23,7 +23,7 @@ namespace APIContagem.Controllers
                 {
                     _CONTADOR.ValorAtual,
                     Environment.MachineName,
-                    Local = "GitHub + Azure Web App for Containers",
+                    Local = "GitHub + Azure Web App for Containers...",
                     Sistema = Environment.OSVersion.VersionString,
                     Variavel = configuration["TesteAmbiente"],
                     TargetFramework = Assembly
